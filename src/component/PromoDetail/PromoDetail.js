6448fcb3c5a36cf7bf83642b992e9bdc46a72411
Jin <jinson@uw.edu>
2021-06-13 23:59:03 -0700

    finished the larger size screen

--- src/component/PromoDetail/PromoDetail.js
+++ src/component/PromoDetail/PromoDetail.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
@@ -10,11 +10,23 @@ const PromoDetail = (props) => {
   let data = props.location.state.data;
   let viewCount = props.location.state.viewCount;
 
-  console.log(props.location.state.data);
-  console.log(data.img);
+  const [isLargeScreenSize, setIsLargeScreenSize] = useState(
+    window.innerWidth >= 1200
+  );
 
-  return (
-    <>
+  const updateScreenSize = () => {
+    setIsLargeScreenSize(window.innerWidth >= 1200);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateScreenSize);
+    return () => {
+      window.removeEventListener("resize", updateScreenSize);
+    };
+  });
+
+  const smallScreenSizeContent = () => {
+    return (
       <div className="promo-det-container">
         <div className="card-img-container">
           <img className="card-img" src={`../../img/${data.img}`} alt="" />
@@ -51,6 +63,56 @@ const PromoDetail = (props) => {
         </div>
         <div className="card-btn">Order Now</div>
       </div>
+    );
+  };
+
+  const largeScreenSizeContent = () => {
+    return (
+      <div className="promo-det-container">
+        <div className="card-img-container">
+          <img className="card-img" src={`../../img/${data.img}`} alt="" />
+          <span className="promo-minor-info">
+            <div className="location">{data.location}</div>
+
+            <div className="promo-viewCount">
+              <FontAwesomeIcon className="icon" icon={faEye} />
+              <span className="text">{viewCount}</span>
+            </div>
+          </span>
+        </div>
+
+        <div className="promo-info-container">
+          <div className="promo-info">
+            <div className="promo-type">{data.promotionType}</div>
+            <div className="store-name">{data.storeName}</div>
+            <div className="divider"></div>
+
+            <div className="title">{data.title}</div>
+            <div className="subtitle">{data.subtitle}</div>
+            <div className="det-descr">{data.detailedDescr}</div>
+            <div className="promo-store-add-info">
+              <div>
+                Address:
+                <a href={`http://maps.google.com/?q=${data.addr}`}>
+                  {" "}
+                  {data.addr}
+                </a>
+              </div>
+
+              <div>
+                Phone: <a href={`tel:${data.phone}`}>{data.phone}</a>
+              </div>
+            </div>
+          </div>
+          <div className="card-btn">Order Now</div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      {isLargeScreenSize ? largeScreenSizeContent() : smallScreenSizeContent()}
     </>
   );
 };
